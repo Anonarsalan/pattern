0fc223d25c64bda1cd79455fbec4098e84c801d8
Anonarsalan <96664174+Anonarsalan@users.noreply.github.com>
2022-12-05 00:51:09 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+        System.out.println("enter no. of rows");
+        int n= sc.nextInt();
+        int i;
+        for ( i=1;i<=n;i+=1){
+            for(int j=4;j<=n;j--){
+            System.out.print("*");
+
+                }
+            }
+            System.out.println("\n");
+
+        }
+    }
+
